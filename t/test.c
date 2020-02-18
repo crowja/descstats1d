62bc293fb4c166c7422236c31c8ac2bbf7c557d7
jac <crowja@gmail.com>
2020-02-17 20:32:53 -0600

    Using tinyhelp.h.

--- t/test.c
+++ t/test.c
@@ -4,39 +4,7 @@
 #include <math.h>
 #include "descstats1d.h"
 #include "tinytest.h"
-
-
-#ifdef COLOR_CODE
-#undef COLOR_CODE
-#endif
-#define COLOR_CODE      0x1B
-#ifdef COLOR_RED
-#undef COLOR_RED
-#endif
-#define COLOR_RED       "[1;31m"
-#ifdef COLOR_GREEN
-#undef COLOR_GREEN
-#endif
-#define COLOR_GREEN     "[1;32m"
-#ifdef COLOR_YELLOW
-#undef COLOR_YELLOW
-#endif
-#define COLOR_YELLOW    "[1;33m"
-#ifdef COLOR_RESET
-#undef COLOR_RESET
-#endif
-#define COLOR_RESET     "[0m"
-
-static void
-printf_test_name(char *name, char *info)
-{
-   printf("%c%s%s%c%s", COLOR_CODE, COLOR_YELLOW, name, COLOR_CODE, COLOR_RESET);
-
-   if (NULL != info)
-      printf(" [%s]\n", info);
-   else
-      printf("\n");
-}
+#include "tinyhelp.h"
 
 static void
 test_cmean(void)
@@ -47,20 +15,16 @@ test_cmean(void)
    double     *wrk = malloc(9 * sizeof(double));
    double      mean, var;
 
-   printf_test_name("test_cmean()", NULL);
-
+   fprintf_test_info(stdout, "test_cmean", NULL);
    descstats1d_cmean(9, x, wrk, &mean, &var);
-   ASSERT("cmean x test mean", fabs(mean - 0.0) < 4 * DBL_EPSILON);
-   ASSERT("cmean x test var ", fabs(var - 28.0 / 7) < 4 * DBL_EPSILON);
-
+   ASSERT_DOUBLE_EQUALS(0.0, mean);
+   ASSERT_DOUBLE_EQUALS(28.0 / 7, var);
    descstats1d_cmean(8, y, wrk, &mean, &var);
-   ASSERT("cmean y test mean", fabs(mean - 0.0) < 4 * DBL_EPSILON);
-   ASSERT("cmean y test var ", fabs(var - 28.0 / 6) < 4 * DBL_EPSILON);
-
+   ASSERT_DOUBLE_EQUALS(0.0, mean);
+   ASSERT_DOUBLE_EQUALS(28.0 / 6, var);
    descstats1d_cmean(8, z, wrk, &mean, &var);
-   ASSERT("cmean shuffled y test mean", fabs(mean - 0.0) < 4 * DBL_EPSILON);
-   ASSERT("cmean shuffled y test var ", fabs(var - 28.0 / 6) < 4 * DBL_EPSILON);
-
+   ASSERT_DOUBLE_EQUALS(0.0, mean);
+   ASSERT_DOUBLE_EQUALS(28.0 / 6, var);
    free(wrk);
 }
 
@@ -71,15 +35,13 @@ test_mean(void)
    double      y[8] = { 4, 3, 2, 1, -1, -2, -3, -4 };
    double      mean, var;
 
-   printf_test_name("test_mean()", NULL);
-
+   fprintf_test_info(stdout, "test_mean", NULL);
    descstats1d_mean(9, x, &mean, &var);
-   ASSERT("mean x test mean", fabs(mean - 0.0) < 4 * DBL_EPSILON);
-   ASSERT("mean x test var ", fabs(var - 60.0 / 8) < 4 * DBL_EPSILON);
-
+   ASSERT_DOUBLE_EQUALS(0.0, mean);
+   ASSERT_DOUBLE_EQUALS(60.0 / 8, var);
    descstats1d_mean(8, y, &mean, &var);
-   ASSERT("mean y test mean", fabs(mean - 0.0) < 4 * DBL_EPSILON);
-   ASSERT("mean y test var ", fabs(var - 60.0 / 7) < 4 * DBL_EPSILON);
+   ASSERT_DOUBLE_EQUALS(0.0, mean);
+   ASSERT_DOUBLE_EQUALS(60.0 / 7, var);
 }
 
 static void
@@ -90,16 +52,13 @@ test_median(void)
    double      median, mad;
    double     *wrk = malloc(9 * sizeof(double));
 
-   printf_test_name("test_median()", NULL);
-
+   fprintf_test_info(stdout, "test_median", NULL);
    descstats1d_median(9, x, wrk, &median, &mad, 0);
-   ASSERT("median x test median", fabs(median - 0.0) < 4 * DBL_EPSILON);
-   ASSERT("median x test mad   ", fabs(mad - 2.0) < 4 * DBL_EPSILON);
-
+   ASSERT_DOUBLE_EQUALS(0.0, median);
+   ASSERT_DOUBLE_EQUALS(2.0, mad);
    descstats1d_median(8, y, wrk, &median, &mad, 0);
-   ASSERT("median y test median", fabs(median - 0.0) < 4 * DBL_EPSILON);
-   ASSERT("median y test mad   ", fabs(mad - 2.5) < 4 * DBL_EPSILON);
-
+   ASSERT_DOUBLE_EQUALS(0.0, median);
+   ASSERT_DOUBLE_EQUALS(2.5, mad);
    free(wrk);
 }
 
@@ -110,15 +69,12 @@ test_min_max(void)
    double     *x = malloc(sizeof(double) * 10000);
    double      max, min;
 
-   printf_test_name("test_min_max()", NULL);
-
+   fprintf_test_info(stdout, "test_min_max", NULL);
    for (i = 0; i < 10000; i++)
       x[i] = 0.5 * (i % 5 - 2);
-
    descstats1d_min_max(10000, x, &min, &max);
-
-   ASSERT(0, (min == -1) && (max == 1));
-
+   ASSERT_EQUALS(-1.0, min);
+   ASSERT_EQUALS(1.0, max);
    free(x);
 }
 
@@ -127,35 +83,23 @@ test_order(void)
 {
    double      x[9] = { 4, 3, 2, 1, 0, -1, -2, -3, -4 };
 
-   printf_test_name("test_order()", NULL);
-
+   fprintf_test_info(stdout, "test_order", NULL);
    descstats1d_order(9, x);
-
-#if 0
-   {
-      int         i;
-      for (i = 0; i < 9; i++)
-         printf("\t%f\n", x[i]);
-   }
-#endif
-
-   ASSERT_EQUALS(-4, x[0]);
-   ASSERT_EQUALS(0, x[4]);
-   ASSERT_EQUALS(4, x[8]);
+   ASSERT_DOUBLE_EQUALS(-4.0, x[0]);
+   ASSERT_DOUBLE_EQUALS(0.0, x[4]);
+   ASSERT_DOUBLE_EQUALS(4.0, x[8]);
 }
 
-#if 0                                            /* 12 yy */
+#if 0                                            /* 10 yy */
 static void
 test_stub(void)
 {
    int         i;
    double      x[9] = { 4, 3, 2, 1, 0, -1, -2, -3, -4 };
 
-   printf_test_name("test_order()", NULL);
-
+   fprintf_test_info(stderr, "test_order", NULL);
    descstats1d_order(9, x);
-
-   ASSERT_EQUALS(-4, x[0]);
+   ASSERT_DOUBLE_EQUALS(-4.0, x[0]);
 }
 #endif
 
